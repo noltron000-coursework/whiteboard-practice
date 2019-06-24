706f80405a8880d12185c777bf4bee348cfde614
Nolan Kovacik <noltron000@gmail.com>
2019-06-24 00:28:17 -0700

    forgot second loop

--- source/unique.py
+++ source/unique.py
@@ -1,14 +1,14 @@
-'''
-	Find the longest substring of unique
-	letters in a given string of n letters.
-'''
 
-function(my_string)
-	my_string.tolower()
-	best_strings = []
-	i = 0 // 1st index in string
+def unique_sub(string)
+	'''
+		Find the longest substring of unique
+		letters in a given string of n letters.
+	'''
+	i = 0 # primary string index
 	while i < length(my_string); i += 1
-		if my_string@[i] = my_string@[i+j]
+		j = i+1 # secondary string index
+		while j < length(my_string); j += 1
+		if my_string@[i] = my_string@[i+j+1]
 			best_strings -> append(my_string[i])
 	find best string in my string
 	return it
