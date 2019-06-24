711e3025d977e8a0727fc3e7c599b0d4b52b17cc
Nolan Kovacik <noltron000@gmail.com>
2019-06-24 00:46:17 -0700

    first iteration of unique_substring
    written in python; it works well enough. no test cases made yet.

--- source/unique.py
+++ source/unique.py
@@ -1,14 +1,39 @@
-
-def unique_sub(string)
+def unique_sub(string):
 	'''
 		Find the longest substring of unique
 		letters in a given string of n letters.
 	'''
-	i = 0 # primary string index
-	while i < length(my_string); i += 1
-		j = i+1 # secondary string index
-		while j < length(my_string); j += 1
-		if my_string@[i] = my_string@[i+j+1]
-			best_strings -> append(my_string[i])
-	find best string in my string
-	return it
+	# keep track of longest substring here
+	best_sub = ''
+	# iterate through each character in the string
+	i = 0  # primary string index / iterator
+	while i < len(string):
+		# create character-storage unit; a set
+		char_set = {}
+		# again - iterate through each character in the string
+		# that is located after the primary index, i
+		j = 0  # secondary string index / iterator
+		while i+j < len(string):
+			# check if the char_set contains given character...
+			if string[i+j] in char_set:
+				# ...if it does, we will have to break this loop
+				# but first, we can see if the substring is long
+				if len(string[i:i+j]) > len(best_sub):
+					# it is a long substring :D
+					best_sub = string[i:i+j]
+				# let's get out of this nested loop
+				break
+			else:
+				# add indexed location to character-storage unit
+				char_set[string[i+j]] = None # value is None
+			j += 1  # iterate secondary index
+		i += 1  # iterate primary index
+		# unfortunately, there is no good way to break out
+		# of this main loop early that I could find
+	return 'LONGEST STRING:\n' + best_sub
+
+
+mega_string = 'abcdefgghijklmnopqr yesss'
+
+
+print(unique_sub(mega_string))
